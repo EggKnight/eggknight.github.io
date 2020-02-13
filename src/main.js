006fcd8b94c9da66688c0860af3e1ab7eda0e87c
EggKnight <61011974+EggKnight@users.noreply.github.com>
2020-02-13 15:00:50 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
-//const textureSource = require("img/texture.json");
+//Putting this all in a big try-catch function because my school disables chrome dev tools
 
-//Creates new PIXI application
-const app = new PIXI.Application();
+try {
+  //Creates new PIXI application
+  const app = new PIXI.Application();
 
-//Add PIXI to document
-document.body.appendChild(app.view);
+  //Add PIXI to document
+  document.body.appendChild(app.view);
 
+  //Load images
+  PIXI.loader
+    .add()
+    .load(loadTextures);
 
-let type = "WebGL";
-if(!PIXI.utils.isWebGLSupported()){
-  type = "canvas";
+  //Object where sprites are contained
+  var sprites = {};
+
+  //Loads sprites
+  var loadTextures = function(){
+    for(var t in PIXI.loader.resources){
+      let name = t.replace(".jpg", "").replace(".png", "");
+      sprites.name = t.texture;
+    }
+  }
+} catch( e ){
+  //Add error as a paragraph to the screen
+  let p = document.createElement("p");
+  p.innerHTML = e;
+  document.body.appendChild(p);
+  p.innerHTML = sprites;
+  document.body.appendChild(p);
+  p.innerHTML = PIXI.loader.resources;
 }
-
-PIXI.utils.sayHello(type);
-
-app.renderer.backgroundColor = 0x03ff40;
 
