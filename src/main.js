fb7baecbae9bf205323f50a14452a3ac2c338a9e
Ethan <32781474+DinamicDigital@users.noreply.github.com>
2020-02-13 13:04:36 -0500

    seeing if background changes
    app.renderer.backgroundColor

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const textureSource = require("img/texture.json");
 
 //Creates new PIXI application
 const app = new PIXI.Application();
+app.renderer.backgroundColor = 0x03ff40;
 
 //Add PIXI to document
 document.body.appendChild(app.view);
