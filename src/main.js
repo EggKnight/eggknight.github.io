dd2749896679a7d168201ddc63fd5a315ce68d59
Ethan <32781474+DinamicDigital@users.noreply.github.com>
2020-02-13 13:20:19 -0500

    trying to fix no webGL error

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@
 
 //Creates new PIXI application
 const app = new PIXI.Application();
+
+let type = "WebGL";
+if(!PIXI.utils.isWebGLSupported()){
+  type = "canvas";
+}
+
+PIXI.utils.sayHello(type);
+
 app.renderer.backgroundColor = 0x03ff40;
 
 //Add PIXI to document
